beafa2de7975f55b78a2cb35e7925acfab009723
shivam garg <gargshivam482@gmail.com>
2019-04-03 19:50:54 +0530

    almost done

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase :{
+  apiKey: 'AIzaSyDE_C9hq7-WC3vkpb5GUeMSGpuzFQYbW0w',
+  authDomain: 'blood-bank-245a1.firebaseapp.com',
+  databaseURL: 'https://blood-bank-245a1.firebaseio.com',
+  projectId: 'blood-bank-245a1',
+  storageBucket: 'blood-bank-245a1.appspot.com',
+  messagingSenderId: '900180676040'
+}
 };
